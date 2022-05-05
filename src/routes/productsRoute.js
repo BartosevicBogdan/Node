905fa0138fe25f01429a945a0b1fde5068e77af2
BartosevicBogdan <95608837+BartosevicBogdan@users.noreply.github.com>
2022-05-05 14:46:52 +0300

    Feat: added  dbQuery and dbExecute to DB_Helper

--- src/routes/productsRoute.js
+++ src/routes/productsRoute.js
@@ -1,42 +1,75 @@
 const express = require("express");
+const { dbQuery, dbExecute } = require("../utils/DB_Helper");
 const router = express.Router();
 
-router.get("/products", (req, res) => {
+router.get("/products", async (req, res) => {
+  const [dbResponse] = await dbQuery("SELECT * FROM products");
+  console.log("dbResponse", dbResponse);
   const output = {
     success: true,
     msg: "answer to /products GET request endpoint",
+    data: dbResponse,
   };
   res.json(output);
 });
-router.get("/totalproducts", (req, res) => {
+router.get("/totalproducts", async (req, res) => {
+  const [dbResponse] = await dbQuery(
+    `SELECT COUNT(id) AS "Products in table"  FROM products`
+  );
+  console.log("dbResponse", dbResponse);
   const output = {
     success: true,
     msg: "answer to /totalproducts GET request endpoint",
+    data: dbResponse,
   };
   res.json(output);
 });
-router.post("/products", (req, res) => {
+router.post("/products", async (req, res) => {
   const reqData = {
     image_url: req.body.image_url,
     title: req.body.title,
     description: req.body.description,
     price: req.body.price,
   };
-  const output = {
-    success: true,
-    msg: "answer to /products POST request endpoint",
-    action: "create product with information",
-    data: reqData,
-  };
-  res.json(output);
+  // console.log("Object.values(reqData);", Object.values(reqData));
+  const dbResponse = await dbExecute(
+    `INSERT INTO products (image_url, title, description, price) VALUES (?, ?, ?, ?)`,
+    Object.values(reqData),
+    true
+  );
+  // console.log("dbResponse", dbResponse);
+  if (dbResponse === true) {
+    const output = {
+      success: true,
+      msg: "answer to /products POST request endpoint",
+      action: "create product with information",
+      data: reqData,
+    };
+    res.json(output);
+  }
 });
-router.delete("/products/:id", (req, res) => {
-  const output = {
-    success: true,
-    msg: "answer to /products/:id DELETE request endpoint",
-    action: `delete product with ID=${req.params.id}`,
-  };
-  res.json(output);
+router.delete("/products/:id", async (req, res) => {
+  // const reqData = {
+  //   image_url: req.body.image_url,
+  //   title: req.body.title,
+  //   description: req.body.description,
+  //   price: req.body.price,
+  // };
+  // console.log("Object.values(reqData);", Object.values(reqData));
+  const dbResponse = await dbExecute(
+    `DELETE FROM products WHERE id = ? `,
+    [req.params.id],
+    true
+  );
+  // console.log("dbResponse", dbResponse);
+  if (dbResponse === true) {
+    const output = {
+      success: true,
+      msg: "answer to /products/:id DELETE request endpoint",
+      action: `delete product with ID=${req.params.id}`,
+    };
+    res.json(output);
+  }
 });
 
 module.exports = router;
--- src/utils/DB_Helper.js
+++ src/utils/DB_Helper.js
@@ -15,7 +15,37 @@ async function pingDB(printErrorIfFunctionChatchError) {
     }
   }
 }
+async function dbQuery(sql, showLog) {
+  try {
+    showLog && console.log("sql", sql);
+    showLog && typeof sql !== "string" && console.log("sql not a string");
+    const connection = await mysql.createConnection(dbConfig);
+    const dbResponse = await connection.query(sql);
+    await connection.close();
+    return dbResponse;
+  } catch (error) {
+    console.warn("Catched error in DB_Helper/dbQuery()");
+    return false;
+  }
+}
+async function dbExecute(sql, argumentsInArray, showLog) {
+  try {
+    showLog && console.log("sql", sql);
+    showLog && console.log("argumentsInArray", argumentsInArray);
+    showLog && typeof sql !== "string" && console.log("sql not a string");
+    const connection = await mysql.createConnection(dbConfig);
+    const [dbResponse] = await connection.execute(sql, argumentsInArray);
+    showLog && console.log("dbResponse", dbResponse.affectedRows);
+    await connection.close();
+    if (dbResponse.affectedRows === 1) return true;
+  } catch (error) {
+    console.warn("Catched error in DB_Helper/dbExecute()");
+    return false;
+  }
+}
 
 module.exports = {
   pingDB,
+  dbQuery,
+  dbExecute,
 };
